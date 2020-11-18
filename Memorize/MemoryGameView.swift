8a8326a50dc0575caf7519665aaf69fda4bb8113
hoschiho <pascal.hoschi@gmail.com>
2020-11-18 21:55:48 +0100

    integrated bonus time & ugly animations.

--- Memorize/MemoryGameView.swift
+++ Memorize/MemoryGameView.swift
@@ -26,11 +26,11 @@ struct MemoryGameView: View {
             if viewModel.won == true {
                 Text("Du bisch en chline gwünno")
             }
-            if viewModel.score > UserDefaults.standard.integer(forKey: "HighScore"){
+            if viewModel.score > UserDefaults.standard.integer(forKey: "HighScore") {
                 Text("!!!NEW HIGHSCORE!!!")
             }
-            
-            
+
+
             Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
                     withAnimation(.linear(duration: 0.75)) {
@@ -44,13 +44,13 @@ struct MemoryGameView: View {
                 .padding()
                 .foregroundColor(Color.blue)
         }
-        
+
 
     }
-    
 
-    
-    
+
+
+
 }
 
 struct CardView: View {
@@ -62,11 +62,31 @@ struct CardView: View {
         }
     }
 
+    @State private var animatedBonusRemaining: Double = 0
+
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining )) {
+            animatedBonusRemaining = 0
+        }
+    }
+
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(110 - 90), clockwise: true)
+                Group {
+
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(-animatedBonusRemaining * 360 - 90), clockwise: true)
+                            .onAppear {
+                                self.startBonusTimeAnimation()
+                        }
+
+                    } else {
+                        Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(-card.bonusRemaining * 360 - 90), clockwise: true)
+                    }
+                }
                     .padding(5).opacity(0.4)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
@@ -74,13 +94,13 @@ struct CardView: View {
                     .animation(card.isMatched ? Animation.linear.repeatForever(autoreverses: false) : .default)
             }
                 .cardify(isFaceUp: card.isFaceUp)
-            .transition(AnyTransition.scale)
+                .transition(AnyTransition.scale)
 
 
         }
     }
-    
-    
+
+
 
 
     //MARK: - Drawing Constants
